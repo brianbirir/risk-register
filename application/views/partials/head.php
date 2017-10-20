35d866ec93f969620eda66149be67639d5852de3
Brian Birir <brianbirir@gmail.com>
2017-10-20 14:18:09 +0300

    Implement bower package management for CSS & JS libraries

--- application/views/partials/head.php
+++ application/views/partials/head.php
@@ -6,35 +6,37 @@
 	<!-- Tell the browser to be responsive to screen width -->
  	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+
 	<!-- REQUIRED CSS -->
 	
 	<!-- Bootstrap framework -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<!-- Font Awesome -->
- 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/font-awesome/css/font-awesome.min.css">
+ 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
  	<!-- Ionicons -->
- 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/Ionicons/css/ionicons.min.css">
+ 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/Ionicons/css/ionicons.min.css">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/AdminLTE.min.css">
 	<!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/skins/skin-aldea.css"> -->
 	<!-- Vendor CSS -->
-	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/vendor/jquery-ui/themes/ui-lightness/jquery-ui.min.css">
-	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/vendor/iCheck/all.css">
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/bower_components/jquery-ui/themes/ui-lightness/jquery-ui.min.css">
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/bower_components/iCheck/skins/all.css">
 	<!-- Custom CSS -->
 	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/main.css">
+
 
 	<!-- REQUIRED JS SCRIPTS -->
 
 	<!-- jQuery -->
 	<script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
 	<!-- Bootstrap -->
-	<script src="<?php echo base_url(); ?>assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	<!-- AdminLTE App -->
 	<script src="<?php echo base_url(); ?>assets/js/adminlte.min.js"></script>
 	<!-- Vendor JS -->
-	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/vendor/jquery-ui/jquery-ui.min.js"></script>
-	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/vendor/raphael/raphael.min.js"></script>
-	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/vendor/chart.js/Chart.min.js"></script>
-	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/vendor/iCheck/icheck.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/bower_components/parsleyjs/dist/parsley.min.js"></script>
+	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
+	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/raphael/raphael.min.js"></script>
+	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/chart.js/Chart.min.js"></script>
+	<script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/iCheck/icheck.min.js"></script>
+	<script type = 'text/javascript' src="<?php echo base_url(); ?>assets/bower_components/parsleyjs/dist/parsley.min.js"></script>
 </head>
