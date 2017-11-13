92d4b54353578688866275a5b77db5893144074e
Brian Birir <brianbirir@gmail.com>
2017-11-13 20:57:28 +0300

    Remove user information
    * remove notifications
    * remove profile pic from side bar and top navigation

--- application/views/partials/navigation/useraccount.php
+++ application/views/partials/navigation/useraccount.php
@@ -2,7 +2,7 @@
   <!-- Menu Toggle Button -->
   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
     <!-- The user image in the navbar-->
-    <img src="<?php echo base_url();?>/assets/img/user2-160x160.jpg" class="user-image" alt="User Image">
+    <!-- <img src="<?php echo base_url();?>/assets/img/user2-160x160.jpg" class="user-image" alt="User Image"> -->
     <!-- hidden-xs hides the username on small devices so only the image appears. -->
     <span class="hidden-xs">
       <?php
@@ -13,40 +13,6 @@
   </a>
   
   <ul class="dropdown-menu">
-    <!-- The user image in the menu -->
-    <li class="user-header">
-      <img src="<?php echo base_url();?>/assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-      <p>
-        <?php  echo $currentUser; ?>
-        <!--<small>Member since Nov. 2012</small>-->
-      </p>
-    </li>
-    <!-- Menu Body -->
-    <li class="user-body">
-      <div class="row">
-        <div class="col-xs-4 text-center">
-          <a href="#">Followers</a>
-        </div>
-        <div class="col-xs-4 text-center">
-          <a href="#">Sales</a>
-        </div>
-        <div class="col-xs-4 text-center">
-          <a href="#">Friends</a>
-        </div>
-      </div>
-      <!-- /.row -->
-    </li>
-    <!-- Menu Footer-->
-    <li class="user-footer">
-      <div class="pull-left">
-        <!-- <a href="#" class="btn btn-default btn-flat">Profile</a> -->
-        <?php echo anchor('profile/'.$user_id,'Profile',array('title'=>'profile','class'=>'btn btn-default btn-flat'))?>
-      </div>
-      <div class="pull-right">
-        <!-- <a href="#" class="btn btn-default btn-flat">Sign out</a> -->
-        <?php echo anchor('logout','Sign Out',array('title'=>'logout','class'=>'btn btn-default btn-flat'))?>
-      </div>
-    </li>
+    <li><?php echo anchor('logout','Sign Out',array('title'=>'logout'))?></li>
   </ul>
 </li>
