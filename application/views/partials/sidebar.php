08094b4d7e4341779566f89f347ef68a20d5f82f
Brian Birir <brianbirir@gmail.com>
2018-02-01 23:22:50 +0300

    FIX: Sidebar hamburger menu was non-functional
    * Now works well in mobile view but hidden when viewed from desktop

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -5,27 +5,10 @@
       <ul class="sidebar-menu" data-widget="tree">
         <li><a href="<?php echo base_url(); ?>dashboard"><i class="fa fa-line-chart"></i>Dashboard</a></li>  
         <li><a href="<?php echo base_url(); ?>dashboard/project"><i class="fa fa-folder"></i>Projects</a></li>
-        <!-- <li><a href="<?php // echo base_url(); ?>dashboard/riskregisters"><i class="fa fa-folder"></i>Risk Registers</a></li> -->
-        <!-- <li><a href="<?php // echo base_url(); ?>dashboard/risks"><i class="fa fa-folder"></i>Risks</a></li> -->
-        <!-- <li class="treeview">
-            <a href="#">
-              <i class="fa fa-pie-chart"></i>
-              <span>Risks</span>
-              <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-              <ul class="treeview-menu" style="display: none;">
-                <li><a href="<?php // echo base_url(); ?>dashboard/risks"><i class="fa fa-file"></i>Current Risks</a></li>
-                <li><a href="<?php // echo base_url(); ?>dashboard/risks/archived"><i class="fa fa-trash"></i>Archived Risks</a></li>
-              </ul>
-            </a>
-        </li> -->
-
         <li><a href="<?php echo base_url(); ?>dashboard/reports"><i class="fa fa-folder"></i>Reports</a></li>
         <?php if ($role_id != 8) { ?>
           <li><a href="<?php echo base_url(); ?>dashboard/settings"><i class="fa fa-cog"></i>Settings</a></li>
-        <?php } ?>   
-        
+        <?php } ?>
       </ul>
       <!-- /.sidebar-menu -->
     </section>
--- application/views/templates/dashboard.php
+++ application/views/templates/dashboard.php
@@ -5,14 +5,14 @@
     <?php $this->load->view('partials/head'); ?>
 
     <!-- Body Section -->
-    <body id="dasboard-body" class="sidebar-mini">
+    <body id="dasboard-body" class="hold-transition sidebar-mini">
 
         <div class="wrapper">
             <!-- header -->
             <?php $this->load->view('partials/header'); ?>
 
-              <!-- dashboard side bar -->
-              <?php $this->load->view('partials/sidebar'); ?>
+            <!-- dashboard side bar -->
+            <?php $this->load->view('partials/sidebar'); ?>
 
               <!-- Content Wrapper. Contains page content -->
               <div class="content-wrapper">
@@ -34,10 +34,13 @@
 
         <!-- jQuery -->
         <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+        
         <!-- Bootstrap -->
         <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        
         <!-- AdminLTE App -->
         <script src="<?php echo base_url(); ?>assets/js/adminlte.min.js"></script>
+        
         <!-- Vendor JS -->
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/moment/moment.js"></script>
@@ -46,8 +49,9 @@
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/iCheck/icheck.min.js"></script>
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/parsleyjs/dist/parsley.min.js"></script>
         <!-- <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script> -->
+        
         <!-- Custom js -->
-	      <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/js/main.js"></script>
+        <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/js/main.js"></script>
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/js/date.js"></script>
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/js/validate.js"></script>
         <script type = 'text/javascript' src = "<?php echo base_url(); ?>assets/js/register-tabs.js"></script>
