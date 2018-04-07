1631ba7e9b0b52c08fee7540785f2883b44f81b7
Brian Birir <brianbirir@gmail.com>
2018-04-07 22:03:05 +0300

    Add redis config file

---
+++ application/config/redis.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['host'] = 'localhost';
+$config['port'] = 6379;
+$config['scheme'] = 'tcp';
+$config['persistent'] = "1";
