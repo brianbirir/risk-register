d3b47f75e188619fc8b05f5bab68bad4adae8ce8
Brian Birir <brianbirir@gmail.com>
2018-04-10 19:07:31 +0300

    Add session data placeholders to sidebar
    * Register name
    * Project name

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -1,15 +1,30 @@
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
+      
+      <!-- Session Data -->
+      <div class="session-data">
+        <ul class="sidebar-menu">
+          <li>
+            <span>Current Project:</span>
+            <span class="session-name"><small class="label bg-red">Project Name</small></span>
+          </li>
+          <li>
+            <span>Current Register:</span>
+            <span class="session-name"><small class="label bg-red">Register Name</small></span>
+          </li>
+        </ul>
+      </div>
+
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li><a href="<?php echo base_url(); ?>dashboard"><i class="fa fa-line-chart"></i>Dashboard</a></li>  
-        <li><a href="<?php echo base_url(); ?>dashboard/project"><i class="fa fa-folder"></i>Projects</a></li>
+        <li><a href="<?php echo base_url(); ?>dashboard"><i class="fas fa-chart-pie"></i>Dashboard</a></li>  
+        <li><a href="<?php echo base_url(); ?>dashboard/project"><i class="fas fa-folder"></i>Projects</a></li>
         <li class="treeview">
           <a href="#">
             <i class="far fa-newspaper"></i> <span>Reports</span>
             <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
+              <i class="fas fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
