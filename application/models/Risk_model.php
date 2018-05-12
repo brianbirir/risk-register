f13127a9c0713b3e916484f03b54b4710be8dc41
Brian Birir <brianbirir@gmail.com>
2018-05-13 01:27:43 +0300

    Remove defunct risk model archive GET functions

--- application/models/Risk_model.php
+++ application/models/Risk_model.php
@@ -225,30 +225,7 @@
             $query = $this->db->get();
             return ($query->num_rows() > 0) ? $query->result() : false;
         }
-
-
-        // get archived risk items
-        function getUserArchivedRisk($user_id)
-        {   
-            $this->db->select('*');
-            $this->db->from('RiskRegistry');
-            $this->db->where('User_user_id',$user_id);
-            $this->db->where('archived',true);
-            $query = $this->db->get();
-            return ($query->num_rows() > 0) ? $query->result() : false;
-        }
         
-
-        function getManagerArchivedRisk( $user_id )
-        {
-            $this->db->select('*');
-            $this->db->from('RiskRegistry');
-            $this->db->join('User','User.user_id = RiskRegistry.User_user_id');
-            $this->db->where('User.parent_user_id',$user_id); // equivalent to parent user id
-            $this->db->where('RiskRegistry.archived',true); // archived
-            $query = $this->db->get();
-            return ($query->num_rows() > 0) ? $query->result() : false;
-        }
 
         // get single risk item based on risk ID
         function getRisk($risk_id)
