70f531713a8b76d8e95d507ee20c39be9d747552
Brian Birir <brianbirir@gmail.com>
2018-10-14 10:00:53 +0300

    Make code readable

--- application/libraries/Riskmatrix.php
+++ application/libraries/Riskmatrix.php
@@ -10,13 +10,16 @@ class RiskMatrix
         if ($risk_rating >= 11 && $risk_rating <= 25)
         {
             $label_color = 'label-danger';
-        } else if($risk_rating >= 4 && $risk_rating <= 10) 
+        } 
+        else if($risk_rating >= 4 && $risk_rating <= 10) 
         {
             $label_color = 'label-warning';
-        } else if ($risk_rating >= 1 && $risk_rating <= 3) 
+        } 
+        else if ($risk_rating >= 1 && $risk_rating <= 3) 
         {
             $label_color = 'label-success';
-        } else if ($risk_rating == 0) 
+        } 
+        else if ($risk_rating == 0) 
         {
             $label_color = 'label-default';
         }
