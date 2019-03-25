a6cc031b723cf1f46bf908d7bef12acc3f972112
brianbirir <brianbirir@gmail.com>
2019-03-25 21:56:09 +0300

    Add provisioning script and update vagrant configs

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,14 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+$script = <<-SCRIPT
+echo copying NGINX configuration file
+cp -ap /vagrant/server_config/nginx/server.conf /etc/nginx/conf.d
+cp -ap /vagrant/server_config/nginx/phpmyadmin.conf /etc/nginx/conf.d
+echo Restart NGINX
+systemctl restart nginx
+SCRIPT
+
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   # config.vm.network :forwarded_port, guest: 8080, host: 5000
@@ -8,4 +16,8 @@ Vagrant.configure("2") do |config|
   config.vm.network :forwarded_port, guest: 3009, host: 9003 # phpmyadmin
   config.vm.network :forwarded_port, guest: 3306, host: 6033 # mysql
   config.vm.synced_folder "", "/var/www/risk-register"
+
+  # provision with shell scripts
+  config.vm.provision "shell", path: "provision.sh"
+  config.vm.provision "shell", inline: $script
 end
---
+++ provision.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+echo "update packages"
+apt-get -y update
+
+# install NGINX
+echo "Install Nginx"
+apt-get -y install nginx
+
+# install php
+apt-get -y install php php-mysql php-fpm
+
+echo "Install MySQL"
+# ---------------------------------------
+#          MySQL Setup
+# ---------------------------------------
+
+# TURN OFF FRONT END PROMPTS FOR MySQL and PHPMYADMIN
+export DEBIAN_FRONTEND="noninteractive"
+
+
+# Setting MySQL root user password
+echo "mysql-server-5.6 mysql-server/root_password password toor" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password toor" | sudo debconf-set-selections
+
+
+# Installing packages
+apt-get install -y mysql-server mysql-client
+
+# Allow External Connections on your MySQL Service
+sudo sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+mysql -u root -ptoor -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH privileges;"
+sudo service mysql restart
+
+# create client database
+echo "create client database"
+DATABASE_NAME="riskregister_prod"
+mysql -u root -ptoor -e "CREATE DATABASE $DATABASE_NAME;"
+
+echo "import risk register database"
+mysql -u root -ptoor $DATABASE_NAME < /vagrant/riskregister.sql
+
+
+# ---------------------------------------
+#          PHPMYADMIN Setup
+# ---------------------------------------
+
+echo "set up phpmyadmin"
+
+# install phpmyadmin
+APP_PASS="toor"
+ROOT_PASS="toor"
+APP_DB_PASS="toor"
+
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ROOT_PASS" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS" | sudo debconf-set-selections
+# echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
+
+apt-get install -y phpmyadmin
